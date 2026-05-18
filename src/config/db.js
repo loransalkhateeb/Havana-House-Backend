9d4d515f4404fe0f86a4fbdb5b598f1f84d51307
Lorans Alkhateeb <loransmahmoodalkhateeb@gmail.com>
2026-05-18 11:45:52 -0700

    Edit

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,19 @@
 const { PrismaClient } = require("@prisma/client");
 const path = require("path");
 
-const isPackaged = !process.defaultApp;
-const dbPath = isPackaged
-  ? path.join(path.dirname(process.execPath), 'dev.db')
-  : path.join(__dirname, '../../prisma/dev.db');
+// Determine the correct database path based on runtime environment
+let dbPath;
+
+if (process.versions.electron) {
+  // Running inside Electron
+  const isPackaged = !process.defaultApp;
+  dbPath = isPackaged
+    ? path.join(path.dirname(process.execPath), 'dev.db')
+    : path.join(__dirname, '../../prisma/dev.db');
+} else {
+  // Running as plain Node.js (npm run dev / npm start)
+  dbPath = path.join(__dirname, '../../prisma/dev.db');
+}
 
 const prisma = new PrismaClient({
   datasources: {
